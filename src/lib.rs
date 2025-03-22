d6cca8d348f54c43800019ec65ad49dca4b9c7a2
HAMEL <75175897+MatthieuHAMEL@users.noreply.github.com>
2025-03-22 11:35:29 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, LitStr};
-use serde_json;
 
 #[proc_macro]
 pub fn generate_sprites(input: TokenStream) -> TokenStream {
